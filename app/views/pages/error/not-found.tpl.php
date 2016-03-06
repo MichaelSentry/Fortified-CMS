fd9e01e9643c359edabef854ac78e24504a738dc
MichaelSentry <gitsentry@ninjasentry.com>
2016-03-06 15:25:30 +1100

    Add example 404 page

---
+++ app/views/pages/error/not-found.tpl.php
@@ -0,0 +1,5 @@
+<h2>Not Found</h2>
+<div class="inner-content">
+    <p>Sorry, the page you're looking could not be found. It may have been moved or never actually existed.</p>
+    <p>Try the search or <a href="<?php echo $this->httpPath;?>" title="Return to the homepage">return to the homepage</p>
+</div>
